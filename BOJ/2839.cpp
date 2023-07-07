f5135dc514a2e33d22897023d1a7a4adbf692ae1
hwannow <vnfdlvgid061@naver.com>
2023-07-07 21:52:29 +0900

    solving BOJ 2839

--- BOJ/2839.cpp
+++ BOJ/2839.cpp
@@ -3,8 +3,40 @@
 #define y second
 using namespace std;
 
-int main() {
-	cout << "Hello World!\n";
+int arr[5005];
 
-	return 0;
+int main() {
+	int n, five = 0, three = 0;
+	cin >> n;
+	if (!(n % 5)) {
+		cout << n / 5;
+		return 0;
+	}
+
+	while (n - 5 >= 0) {
+		n -= 5;
+		five++;
+	}
+	while (1) {
+		n -= 3; three++;
+		for (int i = five; i > 0; i--) {
+			if (n >= 0) break;
+			n += 2;
+			five--; three++;
+		}
+		while (n - 3 >= 0) {
+			n -= 3; three++;
+		}
+
+		if (n <= 0) break;
+
+	}
+
+
+
+	if (n) {
+		cout << "-1";
+		return 0;
+	}
+	cout << five + three;
 }
