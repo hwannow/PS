40c9185cd53237fd404aaece17d3445d5eaafe17
hwannow <vnfdlvgid061@naver.com>
2023-11-14 13:56:28 +0900

    resolving BOJ 23747

--- BOJ/23747.cpp
+++ BOJ/23747.cpp
@@ -3,13 +3,10 @@
 #define y second
 using namespace std;
 
-int n, m;
-int a, b;
-char board[1001][1001];
-int visited[1001][1001];
-char answer[1001][1001];
-string str;
+int n, m, a, b;
 int dx[4] = { 1, -1, 0, 0 }, dy[4] = { 0, 0, 1, -1 };
+char board[1001][1001], visited[5001][5001];
+string str;
 
 int range_over(int nx, int ny) {
     return nx < 0 || ny < 0 || nx >= n || ny >= m;
@@ -18,20 +15,19 @@ int range_over(int nx, int ny) {
 void bfs(int i, int j) {
     char ch = board[i][j];
     queue<pair<int, int>> q;
-    q.push({ i, j });
-    visited[i][j] = 1;
+    q.push({ i ,j });
+    visited[i][j] = '.';
 
     while (!q.empty()) {
         auto cur = q.front(); q.pop();
         for (int dir = 0; dir < 4; dir++) {
             int nx = cur.x + dx[dir], ny = cur.y + dy[dir];
-            if (range_over(nx, ny) || visited[nx][ny] || ch != board[nx][ny]) continue;
-            visited[nx][ny] = 1;
+            if (range_over(nx, ny) || visited[nx][ny] == '.' || board[nx][ny] != ch) continue;
+            visited[nx][ny] = '.';
             q.push({ nx, ny });
         }
     }
 }
-
 
 int main() {
     ios::sync_with_stdio(0);
@@ -42,34 +38,31 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> board[i][j];
-            answer[i][j] = '#';
+            visited[i][j] = '#';
         }
     }
+
     cin >> a >> b;
-    a--, b--;
     cin >> str;
+    a--, b--;
 
     for (int i = 0; i < str.length(); i++) {
-        if (str[i] == 'U') a--;
-        else if (str[i] == 'D') a++;
-        else if (str[i] == 'L') b--;
+        if (str[i] == 'L') b--;
         else if (str[i] == 'R') b++;
-        else {
-            bfs(a, b);
-        }
+        else if (str[i] == 'U') a--;
+        else if (str[i] == 'D') a++;
+        else bfs(a, b);
     }
-    visited[a][b] = 1;
+
+    visited[a][b] = '.';
     for (int dir = 0; dir < 4; dir++) {
         int nx = a + dx[dir], ny = b + dy[dir];
-        if (range_over(nx, ny) || visited[nx][ny]) continue;
-        visited[nx][ny] = 1;
+        visited[nx][ny] = '.';
     }
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (visited[i][j] == 1) cout << '.';
-            else cout << '#';
-        }
+        for (int j = 0; j < m; j++)
+            cout << visited[i][j];
         cout << "\n";
     }
 }
