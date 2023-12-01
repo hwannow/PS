5d6c8e6b124b6b5259d80531193e9c183c51ca6d
hwannow <vnfdlvgid061@naver.com>
2023-12-01 13:58:01 +0900

    solving BOJ 1965

---
+++ BOJ/1965.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define INF 1e9
+using namespace std;
+
+int n, answer = 0;
+int arr[1001], dp[1001];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for (int i = 0; i < n; i++) {
+        int ck = 0;
+        for (int j = 0; j < i; j++) {
+            if (arr[j] < arr[i] && ck < dp[j]) ck = dp[j];
+        }
+        dp[i] = ck + 1;
+        answer = max(answer, dp[i]);
+    }
+    cout << answer;
+}
