27a3cdbe8f4169232a0bdee2e2ae08769314bbf4
hwannow <vnfdlvgid061@naver.com>
2023-12-16 12:16:54 +0900

    solving BOJ 15964

---
+++ BOJ/15964.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define INF 1e9
+using namespace std;
+
+long long n, m;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m;
+    cout << (n + m) * (n - m);
+}
