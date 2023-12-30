4df936d25abf890aea1dabec93a6f8d43d4c770c
hwannow <vnfdlvgid061@naver.com>
2023-12-30 21:57:03 +0900

    solving BOJ 27324

---
+++ BOJ/27324.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define INF 1e9
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string n;
+    cin >> n;
+    int a = n[0] == n[1];
+    cout << a;
+}
