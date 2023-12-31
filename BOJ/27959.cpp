499ce9d444fff73c281a89fbd405ebc46d4d0725
hwannow <vnfdlvgid061@naver.com>
2023-12-31 21:09:22 +0900

    solving BOJ 27959

---
+++ BOJ/27959.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define INF 1e9
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    if (100 * n >= m) cout << "Yes";
+    else cout << "No";
+}
