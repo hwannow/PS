a60c5b7642a278a24ec32c7092ee764c171ae9d8
hwannow <vnfdlvgid061@naver.com>
2024-01-01 23:11:30 +0900

    solving BOJ 30030

---
+++ BOJ/30030.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define INF 1e9
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    cout << n * 10 / 11;
+}
