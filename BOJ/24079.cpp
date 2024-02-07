18a199e727a8db2c5f0e699b68b079de9b5f5564
hwannow <vnfdlvgid061@naver.com>
2024-02-07 19:09:37 +0900

    solving BOJ 24079

---
+++ BOJ/24079.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> a >> b >> c;
+    cout << (a + b <= c);
+}
