eba3aebdb782148ce71715bc8131ef8488b02dd9
hwannow <vnfdlvgid061@naver.com>
2024-02-11 20:18:12 +0900

    solving BOJ 8760

---
+++ BOJ/8760.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+using namespace std;
+
+int n;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        a *= b;
+        cout << a / 2 << "\n";
+    }
+}
