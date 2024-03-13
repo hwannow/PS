2ac26c21527033e595d89f58b72145b536857c3c
hwannow <vnfdlvgid061@naver.com>
2024-03-13 15:04:52 +0900

    solving BOJ 31607

---
+++ BOJ/31607.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+using namespace std;
+
+vector<int> v;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    for (int i = 0; i < 3; i++) {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.begin(), v.end());
+
+    if (v[0] + v[1] == v[2]) cout << 1;
+    else cout << 0;
+}
