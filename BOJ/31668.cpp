c4bcc228c73eba8b79ca934450fc171a260cd3d9
hwannow <vnfdlvgid061@naver.com>
2024-04-13 20:35:17 +0900

    solving BOJ 31668

---
+++ BOJ/31668.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+using namespace std;
+
+int n, m, a;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m >> a;
+    cout << m / n * a;
+}
